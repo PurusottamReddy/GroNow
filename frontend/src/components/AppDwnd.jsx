aaff3869134bee4fa1b3398c56cbdf2bcb59838a
T. Purusottam Reddy <151070326+PurusottamReddy@users.noreply.github.com>
2025-11-05 22:19:57 +0530

    Fix typo

--- frontend/src/components/AppDwnd.jsx
+++ frontend/src/components/AppDwnd.jsx
@@ -5,7 +5,7 @@ export default function AppDwnd() {
         <div className="flex flex-col md:flex-row items-center justify-around text-sm shadow-md rounded-md mt-10 max-w-5xl w-full bg-gray-100">
         
             <div className="flex flex-col text-center md:text-left items-center md:items-start pt-14 md:p-10">
-                <h2 className="md:text-4xl text-2xl font-semibold text-primary">Download GrowNow App</h2>
+                <h2 className="md:text-4xl text-2xl font-semibold text-primary">Download GroNow App</h2>
                 <p className="text-gray-700 mt-2 w-3/4"> Order groceries in seconds, track deliveries in real-time, and enjoy fresh essentials right at your doorstep.</p>
         
                 <div className="flex items-center gap-4 mt-6">
